c32a67265f76225882ef96c6acfd7f6bb8a5f903
rajan0031 <raykushwaha0031@gmail.com>
2025-06-13 11:12:20 +0530

    making a student get request api

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const data = require("./data.js");
+
+app.get("/", (req, res) => {
+  console.log("the data is ", data);
+  res.json( data);
+});
+
+app.listen(3000, (err) => {
+  if (err) {
+    console.log("server error ", err);
+  }
+  console.log(" the server is running at teh port 3000");
+});
